534995d2ff86d89ac32e1cc4f21afe31fb60dbc4
Vasanthan <srvasanthan33@outlook.com>
2023-06-12 13:44:55 +0530

    initila commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require("express")
+const app = express()
+const PORT = 3500
+
+app.get('/',(request,response) =>{
+    response.send("Service Page")
+})
+app.listen(PORT,console.log(`Server running at http://localhost:${PORT}`))
